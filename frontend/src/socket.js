52f6ac96571e68a1aab116ce02d33d1c6c5a50bf
annxiesun <anniesun@v1040-wn-rt-a-1-125.campus-dynamic.uwaterloo.ca>
2024-04-04 15:26:34 -0400

    change ngrok url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -7,7 +7,7 @@ import { myP5Sketch } from ".";
 // const cheatBtn = document.querySelector('#cheatBtn');
 // const goodBtn = document.querySelector('#goodBtn');
 
-const socket = io('https://e0ff-99-209-52-138.ngrok-free.app', {
+const socket = io('https://8ad1-2620-101-f000-700-3dee-6e15-dee0-a4c5.ngrok-free.app', {
   extraHeaders: {
     "ngrok-skip-browser-warning": true
   }
