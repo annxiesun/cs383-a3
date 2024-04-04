d403803b0b55df26c1d7ed6ecf53fd75d6811d6d
annxiesun <anniesun@v1040-wn-rt-a-89-182.campus-dynamic.uwaterloo.ca>
2024-04-04 03:50:50 -0400

    hi

--- frontend/src/sketch.js
+++ frontend/src/sketch.js
@@ -5,43 +5,82 @@ export const OPTIONS = {
   phase: 0,
 };
 
+
 // Exporting a p.called 'mySketch'
 export const mySketch = (p) => {
-  let i = 0
   let ii = 1
 
   let image = null;
+  let images = []
 
   p.setup = () => {
     p.createCanvas(window.innerWidth, window.innerHeight, p.WEBGL);
     p.lights();
 
-    p.camera(0, 0, -800,
+
+    const getRandomSpeed = () => {
+      return p.random(0.005, 0.03)
+    }
+
+    const getRandomHeight = () => {
+      return 180 + p.random(-100,100)
+    }
+
+    p.camera(0, 0, -900,
            0, 0, 0,
-          0, 20, 0);
-      image = p.loadImage('test.PNG'); 
+          0, 1, 0);
+
+    image = p.loadImage('test.PNG'); 
+    images.push({
+      data: image,
+      rotation: 0,
+      rotationCoeff: getRandomSpeed(),
+      height: getRandomHeight(),
+    })
+
+    images.push({
+      data: image,
+      rotation: 0,
+      rotationCoeff: getRandomSpeed(),
+      height: getRandomHeight(),
+    })
+
+    images.push({
+      data: image,
+      rotation: 0,
+      rotationCoeff: getRandomSpeed(),
+      height: getRandomHeight(),
+    })
+
     p.noStroke()
   }
 
   p.draw = () => {
     p.background(0)
 
-    p.translate(0-ii,0,0)
+    images.forEach((imageObj, idx) => {
+      const { data, rotationCoeff } = imageObj
 
-    p.push()
-    p.stroke('#ebb434');
-    p.strokeWeight(2);
-    p.line(0, -200, 0, -1000);
-    p.pop()
-
-    p.push()
-    p.rotateY(20+i)
-    p.texture(image);
-    p.plane(500, 500);
-    p.pop()
+      p.push()
+      p.translate(480-(idx*470),0,0)
+  
+      p.push()
+      p.stroke('#ebb434');
+      p.strokeWeight(2);
+      p.line(0, imageObj.height - 200, 0, -1000);
+      p.pop()
+  
+      p.push()
+      p.rotateY(20+imageObj.rotation)
+      p.texture(data);
+      p.translate(0,imageObj.height)
+      p.plane(450, 450);
+      p.pop()
+      p.pop()
+      imageObj.rotation += rotationCoeff
+    })
 
     ii += 0
-    i += 0.01
   }
   p.mousePressed = () => {
     // p.setTint([255, 0, 0]);
